a42630d5ea63240b012d704fc093f54c44a9a615
Lefti <inserid@gmail.com>
2020-05-21 22:07:14 +0200

    added remainingSeconds excersice

--- week01/day04/firstTask/src/com/company/Main.java
+++ week01/day04/firstTask/src/com/company/Main.java
@@ -199,6 +199,15 @@ public class Main {
         System.out.println("The total volume is " + side1 * side2 * side3);
 
 
+
+
+        // Write a program that prints the remaining seconds (as an integer) from a
+        // day if the current time is represented by the variables
+        int currentHours = 14;
+        int currentMinutes = 34;
+        int currentSeconds = 42;
+        int remainingSeconds = ((23-currentHours)*60 + (59-currentMinutes))*60 + (60 - currentSeconds) ;
+        System.out.println(remainingSeconds);
     }
 }
 
