4b17d6829ae7bb606c5268b823c236dc9208784e
Lefti <inserid@gmail.com>
2020-05-21 14:27:21 +0200

    commit first java tasks i

---
+++ week01/day04/firstTask/src/com/company/Main.java
@@ -0,0 +1,98 @@
+package com.company;
+
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        /* Modify this program to greet you instead of the World!
+        System.out.println("Hello, World!");*/
+
+        System.out.println("Hello Lefti");
+
+
+
+
+        /* Modify this program to print Humpty Dumpty riddle correctly
+        *  System.out.println("All the king's horses and all the king's men");
+        System.out.println("Humpty Dumpty had a great fall.");
+        System.out.println("Humpty Dumpty sat on a wall,");
+        System.out.println("Couldn't put Humpty together again.");*/
+
+        String firstVerse = "Humpty Dumpty sat on a wall,";
+        String  secondVerse = "Humpty Dumpty had a great fall.";
+        String thirdVerse = "All the king's horses and all the king's men";
+        String fourthVerse ="Couldn't put Humpty together again.";
+        System.out.println(firstVerse + "\n" + secondVerse + "\n" + thirdVerse+ "\n" +  fourthVerse );
+
+
+
+
+        // Greet 3 of your classmates with this program, in three separate lines
+        // like:
+        //
+        // Hello, Esther!
+        // Hello, Mary!
+        // Hello, Joe!
+
+        System.out.println("Hello Esther" + "\n" + "Hello Mary" + "\n" + "Hello Joe" );
+
+
+
+        // Write a program that prints a few details to the terminal window about you
+        // It should print each detail to a new line.
+        //  - Your name
+        //  - Your age
+        //  - Your height in meters (as a decimal fraction)
+        //
+        //  Example output:
+        //  John Doe
+        //  31
+        //  1.87
+
+        String name = "Lefti";
+        int age = 35;
+        float height = 1.92f;
+        System.out.println(name + "\n" + age + "\n" + height);
+
+
+
+        // Create a program that prints a few operations on two numbers: 22 and 13
+        // Print the result of 13 added to 22
+        // Print the result of 13 substracted from 22
+        // Print the result of 22 multiplied by 13
+        // Print the result of 22 divided by 13 (as a decimal fraction)
+        // Print the integer part of 22 divided by 13
+        // Print the remainder of 22 divided by 13
+
+        System.out.println( 13 + 22);
+        System.out.println(22-13);
+        System.out.println(22 * 13);
+        System.out.println(22 / 13.);
+        System.out.println(22 / 13);
+        System.out.println( 22 % 13);
+
+
+
+        // An average Green Fox attendee codes 6 hours daily
+        // The semester is 17 weeks long
+        //
+        // Print how many hours is spent with coding in a semester by an attendee,
+        // if the attendee only codes on workdays.
+        //
+        // Print the percentage of the coding hours in the semester if the average
+        // work hours weekly is 52
+
+       int 
+
+
+
+
+
+    }
+}
+
