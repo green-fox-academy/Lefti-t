ea758bb344cfa99239507c18970e1c1b04196d9b
Lefti <inserid@gmail.com>
2020-05-21 17:04:22 +0200

    added one excercise, practising with git(again..)

--- week01/day04/firstTask/src/com/company/Main.java
+++ week01/day04/firstTask/src/com/company/Main.java
@@ -117,6 +117,88 @@ public class Main {
 
 
 
+        // Print the Body mass index (BMI) based on these values
+        double massInKg = 98.2;
+        double heightInM = 1.92;
+        double BMI = massInKg / Math.pow(heightInM,2) ;
+        System.out.println("My BMI is " + BMI + "%, I have to start a diet.");
+
+
+
+
+        // Define several things as a variable then print their values
+        // Your name as a string
+        // Your age as an integer
+        // Your height in meters as a double
+        // Whether you are married or not as a boolean
+
+        String myName = "Lefti";
+        int age1 = 35;
+        double height1 = 1.92;
+        boolean married = false;
+        System.out.println("My name is " + myName + " and i am " + age1 + "years old, " + height1 +  "cm tall and the statement that i am married is " + married + ".");
+
+
+
+        int aa = 3;
+        // make the "a" variable's value bigger by 10
+        aa += 8;
+        System.out.println(aa);
+
+        int bb = 100;
+        // make b smaller by 7
+        bb -= 7;
+        System.out.println(bb);
+
+        int cc = 44;
+        // please double c's value
+        cc *= 2;
+        System.out.println(cc);
+
+        int d = 125;
+        // please divide by 5 d's value
+        d /= 5;
+        System.out.println(d);
+
+        int e = 8;
+        // please cube of e's value
+        e = (int) Math.pow(e,3);
+        System.out.println(e);
+
+        int f1 = 123;
+        int f2 = 345;
+        // tell if f1 is bigger than f2 (print as a boolean)
+        System.out.println( f1 > f2);
+        int g1 = 350;
+        int g2 = 200;
+        // tell if the double of g2 is bigger than g1 (print as a boolean)
+        System.out.println(g2*2 > g1);
+        int h = 135798745;
+        // tell if it has 11 as a divisor (print as a boolean)
+        System.out.println( h%11==0);
+        int i1 = 10;
+        int i2 = 3;
+        // tell if i1 is higher than i2 squared and smaller than i2 cubed (print as a boolean)
+        System.out.println(i1 > Math.pow(i2,2)&&(i1 < Math.pow(i2,3)));
+
+        int j = 1521;
+        // tell if j is dividable by 3 or 5 (print as a boolean)
+        System.out.println( j%3==0 && j%5==0);
+
+
+
+        // Write a program that stores 3 sides of a cuboid as variables (doubles)
+        // The program should write the surface area and volume of the cuboid like:
+        //
+        // Surface Area: 600
+        // Volume: 1000
+        double side1 = 10;
+        double side2 = 10;
+        double side3 = 10;
+        System.out.println("The total surface area is " + (2*side1*side2 + 2*side2*side3 + 2*side1*side3) );
+        System.out.println("The total volume is " + side1 * side2 * side3);
+
+
     }
 }
 
