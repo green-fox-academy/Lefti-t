a1f054b53be4a2ad138ad1f580d94f2d6222077e
Lefti <inserid@gmail.com>
2020-05-22 17:07:47 +0200

    commit more day05 excercises(loops)

--- week01/day05/src/com/company/Main.java
+++ week01/day05/src/com/company/Main.java
@@ -88,7 +88,7 @@ public class Main {
         if ((number01 == number02) && (number01 + number02 >= 20)) {
             System.out.println("The party is awesome!");
         } else if (number01 + number02 >= 20 &&  number01 > number02 || number01 < number02) {
-                System.out.println("The party is quite cool!");
+            System.out.println("The party is quite cool!");
         } else if (number01 + number02 < 20 && number02 > 0) {
             System.out.println("The party is Ok-ish..");
         } else if (number02 == 0) {
@@ -111,45 +111,69 @@ public class Main {
         } else {
             System.out.println("Number is not even");
         }
-        
 
 
 
-        int b = 13;
-        String out2 = "";
         // if b is between 10 and 20 set out2 to "Sweet!"
         // if less than 10 set out2 to "Less!",
         // if more than 20 set out2 to "More!"
+        int b = 13;
+        String out2 = "";
+
+
+        if (b > 10 && b < 20) {
+            out2 = "Sweet!";
+        }else if (b < 10){
+            out2 = "Less!";
+        }else {
+            out2 = "More!";
+        }
+
 
 
         System.out.println(out2);
 
 
 
-        int c = 123;
-        int credits = 100;
-        boolean isBonus = false;
         // if credits are at least 50,
         // and isBonus is false decrement c by 2
         // if credits are smaller than 50,
         // and isBonus is false decrement c by 1
         // if isBonus is true c should remain the same
+        int c = 24;
+        int credits = 100;
+        boolean isBonus = false;
 
+        if ( credits >= 50 && isBonus == false ) {
+            c = c-=2;
+            System.out.println(c);
+        }else if (credits <50 && isBonus == false) {
+            c--;
+            System.out.println(c);
+        }else if (isBonus == true)
 
         System.out.println(c);
 
 
 
-
-        int d = 8;
-        int time = 120;
-        String out3 = "";
         // if d is dividable by 4
         // and time is not more than 200
         // set out3 to "check"
         // if time is more than 200
         // set out3 to "Time out"
         // otherwise set out3 to "Run Forest Run!"
+        int d = 8;
+        int time = 120;
+        String out3 = "";
+
+
+        if ( d%4==0 && time<=200) {
+            out3 = "check";
+        }else if (time >200) {
+            out3 ="Time out";
+        }else {
+            out3 ="Run Forest Run!";
+        }
 
 
         System.out.println(out3);
