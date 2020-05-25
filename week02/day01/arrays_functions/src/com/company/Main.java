4c07c5abfc254808f58e550c1267f57f6868bf76
Lefti <inserid@gmail.com>
2020-05-25 14:14:24 +0200

    added week02day01 arrays

---
+++ week02/day01/arrays_functions/src/com/company/Main.java
@@ -0,0 +1,111 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        // - Create an array variable named `numbers`
+        //   with the following content: `[4, 5, 6, 7]`
+        // - Print the third element of `numbers`
+
+
+        int [] numbers = {4,5,6,7};
+        System.out.println(numbers[2]);
+
+
+
+        // - Create an array variable named `firstArrayOfNumbers`
+        //   with the following content: `[1, 2, 3]`
+        // - Create an array variable named `secondArrayOfNumbers`
+        //   with the following content: `[4, 5]`
+        // - Print "secondArrayOfNumbers is longer" if `secondArrayOfNumbers` has more
+        //   elements than `firstArrayOfNumbers`
+
+
+
+        int[] firstArrayofNumbers = {1,2,3};
+        int []secondArrayofNumbers = {4,5};
+        if ( secondArrayofNumbers.length > firstArrayofNumbers.length)
+        System.out.println("secondArrayOfNumbers is longer");
+
+
+
+        // - Create an array variable named `numbers`
+        //   with the following content: `[54, 23, 66, 12]`
+        // - Print the sum of the second and the third element
+
+
+
+        int [] numbers2 = {54,23,66,12};
+        System.out.println(numbers2[2] + numbers2[3]);
+
+
+        // - Create an array variable named `numbers`
+        //   with the following content: `[1, 2, 3, 8, 5, 6]`
+        // - Change the 8 to 4
+        // - Print the fourth element
+
+
+        int [] numbers3 = {1,2,3,8,5,6};
+        numbers3[3] = 4;
+        System.out.println(numbers3[3]);
+
+
+        // - Create an array variable named `numbers`
+        //   with the following content: `[1, 2, 3, 4, 5]`
+        // - Increment the third element
+        // - Print the third element
+
+
+        int [] numbers4 = {1,2,3,4,5};
+        for (int i = numbers4[2]; i < numbers4[4] ; i++) {
+            System.out.println(numbers4[i]);
+        }
+
+
+
+        // - Create an array variable named `numbers`
+        //   with the following content: `[4, 5, 6, 7]`
+        // - Print all the elements of `numbers`
+
+
+        int []numbers5 = {4,5,6,7};
+        for (int i = 0; i < numbers5.length ; i++) {
+            System.out.println(numbers5[i]);
+        }
+
+
+
+        // - Create (dynamically) a two dimensional array
+        //   with the following matrix. Use a loop!
+        //
+        //   1 0 0 0
+        //   0 1 0 0
+        //   0 0 1 0
+        //   0 0 0 1
+        //
+        // - Print this two dimensional array to the output
+
+
+
+        int [][] twoDimensionalArray  = {{1,0,0,0},{0,1,0,0},{0,0,1,0},{0,0,0,1}};
+
+        for (int i = 0; i < twoDimensionalArray.length ; i++) {
+            System.out.print(twoDimensionalArray[0][i] + " ");
+        }
+        System.out.println();
+        for (int j = 0; j < twoDimensionalArray.length ; j++) {
+            System.out.print(twoDimensionalArray[1][j] + " ");
+        }
+        System.out.println();
+        for (int k = 0; k < twoDimensionalArray.length ; k++) {
+            System.out.print(twoDimensionalArray[2][k] + " ");
+        }
+        System.out.println();
+        for (int l = 0; l < twoDimensionalArray.length ; l++) {
+            System.out.print(twoDimensionalArray[3][l] + " ");
+        }
+        System.out.println();
+    }
+}
