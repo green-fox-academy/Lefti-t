29e430d7c309b42fa972f1d3efe9b21755b6f527
Lefti <inserid@gmail.com>
2020-05-25 14:58:50 +0200

    added week02day01 arrays doubleItems

--- week02/day01/arrays_functions/src/com/company/Main.java
+++ week02/day01/arrays_functions/src/com/company/Main.java
@@ -2,6 +2,9 @@ package com.company;
 
 public class Main {
 
+
+
+
     public static void main(String[] args) {
 
 
@@ -107,5 +110,20 @@ public class Main {
             System.out.print(twoDimensionalArray[3][l] + " ");
         }
         System.out.println();
+
+
+        // - Create an array variable named `numList`
+        //   with the following content: `[3, 4, 5, 6, 7]`
+        // - Double all the values in the array
+
+        int[] numList = {3,4,5,6,7};
+        for (int i = 0; i < numList.length ; i++) {
+            numList[i] *= 2;
+        }
+        for (int j = 0; j < numList.length ; j++) {
+            System.out.println(numList[j]);
+        }
+
+
     }
 }
