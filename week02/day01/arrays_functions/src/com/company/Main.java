a45fac3d840a1197bc2007340a6af9a082015c70
Lefti <inserid@gmail.com>
2020-05-25 16:29:46 +0200

    added week02day01 arrays Reverse(corrected)

--- week02/day01/arrays_functions/src/com/company/Main.java
+++ week02/day01/arrays_functions/src/com/company/Main.java
@@ -183,9 +183,10 @@ public class Main {
         for (int i = 0; i < numbers7.length / 2; i++) {
             int temp2 = numbers7[i];
             numbers7[i] = numbers7[numbers7.length - i - 1];
+            numbers7[numbers7.length - i - 1]= temp2;
         }
-        for (int j = 0; j < numbers7.length; j++) {
-            System.out.println(numbers7[j]);
+        for (int j : numbers7) {
+            System.out.println(j);
         }
     }
 }
