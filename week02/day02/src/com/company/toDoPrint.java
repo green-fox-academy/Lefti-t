a970dc8c304b52fcb1ab1ffc43bed53992fd1637
Lefti <inserid@gmail.com>
2020-05-26 13:49:43 +0200

    added week02day02 todoPrint

--- week02/day02/src/com/company/toDoPrint.java
+++ week02/day02/src/com/company/toDoPrint.java
@@ -1,4 +1,33 @@
 package com.company;
 
 public class toDoPrint {
+
+
+    public static void main(String[] args) {
+
+
+        StringBuffer todoText = new StringBuffer( " - Buy milk\n");
+
+        // Add "My todo:" to the beginning of the todoText
+        // Add " - Download games" to the end of the todoText
+        // Add " - Diablo" to the end of the todoText but with indention
+
+        // Expected output:
+
+        // My todo:
+        //  - Buy milk
+        //  - Download games
+        //      - Diablo
+
+        String myToDo = new String("My todo:\n");
+        String games = new String(" - Download games\n");
+        String diablo = new String("    - Diablo");
+        StringBuffer todoText2 = new StringBuffer(myToDo + todoText + games + diablo);
+        todoText = todoText2;
+
+        System.out.println(todoText);
+
+
+
+    }
 }
