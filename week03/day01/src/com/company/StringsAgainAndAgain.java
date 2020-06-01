9607d684999392ef2a54db6cc3f0008d45960e49
Lefti <inserid@gmail.com>
2020-06-01 16:54:52 +0200

    week03 day01 'StringsAgainAndAgain'

---
+++ week03/day01/src/com/company/StringsAgainAndAgain.java
@@ -0,0 +1,28 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class StringsAgainAndAgain {
+
+    public static void main(String[] args) {
+
+
+        System.out.println("Give me a string");
+        Scanner scanner = new Scanner(System.in);
+        String scan = scanner.nextLine();
+        System.out.println(string(scan));
+
+    }
+
+    public static String string(String s) {
+
+        //Given a string, compute recursively a new string where all the adjacent chars are now separated by a *
+
+        if (s.length() == 1) {
+            return s;
+        } else {
+            return s.charAt(0) + "*" + string(s.substring(1));
+
+        }
+    }
+}
