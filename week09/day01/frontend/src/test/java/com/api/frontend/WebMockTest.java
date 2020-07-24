e02ebe9ccd48c46e298a5ec1d2ba034371d21907
Lefti <inserid@gmail.com>
2020-07-24 15:41:57 +0200

    doUntil testing

--- week09/day01/frontend/src/test/java/com/api/frontend/WebMockTest.java
+++ week09/day01/frontend/src/test/java/com/api/frontend/WebMockTest.java
@@ -2,12 +2,14 @@ package com.api.frontend;
 
 
 import com.api.frontend.controllers.APIController;
+import com.api.frontend.services.FrontendService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -35,7 +37,7 @@ public class WebMockTest {
 
     @Autowired
     private MockMvc mockMvc;
-
+//
 //    @MockBean
 //    private FrontendService frontendService;
 
