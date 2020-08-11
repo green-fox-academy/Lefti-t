3c5f5329d0b74580f8dd3e84cd12bf6d29a989e7
Lefti <inserid@gmail.com>
2020-08-11 17:53:58 +0200

    add  array simple exercises

---
+++ Project_phase/Javascript/week01/arrays/changeElement.js
@@ -0,0 +1,11 @@
+// -  Create an array named `numbers` with the following content: `[1, 2, 3, 8, 5, 6]`
+// -  Change the 8 to 4 with the `.map` method 
+// -  Print the fourth element as a test
+
+let numbers = [1,2,3,8,5,6];
+
+numbers.map((value,index,array) => {
+   return numbers[3] = 4;
+});
+
+console.log( numbers);
--- Project_phase/Javascript/week01/variables/swap/swap.js
+++ Project_phase/Javascript/week01/variables/swap/swap.js
@@ -1,9 +1,7 @@
 let a = 123;
 let b = 526;
 
-let temp = b;
-b = a;
-a = temp;
+[a,b] = [b,a];
 
 console.log(a);
 console.log(b);
